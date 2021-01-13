e587fe3316f80620f463678f5cad3229a3318c35
Dion Johnson <dionjwa@gmail.com>
2021-01-12 21:24:43 -0800

    added new files

---
+++ js/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log('Hello Dion');
+
+// Linter test
+
+
+// Write 5 questions accepting either Y or N in any case
+//examples: Yes, yes, YEs, yeS, yEs, Y, yy
+
+var questionOne = prompt('Does Dion live in the state of WA?');
+
+var normalizedQuestionOne = questionOne.toLocaleLowerCase();
+
+if (normalizedQuestionOne === 'yes' || questionOne === 'y') {
+     // console.log(you are correct')
+      alert('You are Correct')
+
+} else {
+  alert('That is incorrect');
+}
+
+
+let banana = 12
+
+console.log(banana) 
